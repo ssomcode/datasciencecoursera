586520ca80178166ef5d73bc15d3f452cafe3254
omkar shinde <omkarshindes1999@gmail.com>
2019-12-17 09:03:10 +0530

    looping statements

---
+++ looping.R
@@ -0,0 +1,17 @@
+#v=c("hello","loop")
+#cnt=2
+#repeat{
+ # print(v)
+  #cnt=cnt+1
+  #if(cnt>5){
+   # break
+  #}
+#}
+
+z=c("hey","while")
+cnt1=1
+while(cnt1<7){
+  print(z)
+  cnt1=cnt1+1
+}
+  
